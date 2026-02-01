d9ebc7f8f2b7517935c7105d4985864da2f79d7d
Rachel Ramsey <46530671+unitedwestand0@users.noreply.github.com>
2026-01-31 23:52:16 -0600

    created search-agent for openai but ran into some issues along the way, decided to stick with XAi going forward

---
+++ main.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAi
+from tavily import TavilyClient
+
+# from langchain_tavily import TavilySearch
+# updating for commit
+
+tavily = TavilyClient()
+
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches the internet
+
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+
+llm = ChatOpenAi(model="gpt-5")
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="Search for 3 job postings for an Ai Engineer using langchain in Austin, Texas on LinkedIn and list their details"
+            )
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
